fae04398d8ef8eb29c43fe07c1d769cd8ba3ca27
jrarenson <jonathan.arenson44@gmail.com>
2018-11-12 15:35:28 -0500

    database

---
+++ GroupProject/GLM/app/src/main/java/seclass/qc/edu/glm/Database.java
@@ -0,0 +1,22 @@
+package seclass.qc.edu.glm;
+
+public class Database {
+    public String[] getReminderCategory(){
+        String[] ans = null;
+        return ans;
+    }
+
+    public Reminder[] getReminderByCategory(String category){
+        Reminder[] ans = null;
+        return ans;
+    }
+
+    public Reminder[] getReminderByName(String name){
+        Reminder[] ans = null;
+        return ans;
+    }
+
+    public void addReminder(String name, String category){
+
+    }
+}
