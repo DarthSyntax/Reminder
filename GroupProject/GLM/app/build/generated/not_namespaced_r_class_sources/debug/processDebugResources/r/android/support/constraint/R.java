294753eeed8ae5c86dee6a372a47618c4af04c13
Adam vanWestrienen <avanwest@gmail.com>
2018-11-21 17:23:28 -0500

    update UI

--- GroupProject/GLM/app/build/generated/not_namespaced_r_class_sources/debug/processDebugResources/r/android/support/constraint/R.java
+++ GroupProject/GLM/app/build/generated/not_namespaced_r_class_sources/debug/processDebugResources/r/android/support/constraint/R.java
@@ -80,13 +80,13 @@ public final class R {
         public static final int left = 0x7f080068;
         public static final int packed = 0x7f08007f;
         public static final int parent = 0x7f080081;
-        public static final int percent = 0x7f080084;
-        public static final int right = 0x7f08008a;
-        public static final int spread = 0x7f0800ad;
-        public static final int spread_inside = 0x7f0800ae;
-        public static final int start = 0x7f0800b3;
-        public static final int top = 0x7f0800ca;
-        public static final int wrap = 0x7f0800de;
+        public static final int percent = 0x7f080085;
+        public static final int right = 0x7f08008c;
+        public static final int spread = 0x7f0800af;
+        public static final int spread_inside = 0x7f0800b0;
+        public static final int start = 0x7f0800b5;
+        public static final int top = 0x7f0800cc;
+        public static final int wrap = 0x7f0800df;
     }
     public static final class styleable {
         private styleable() {}
