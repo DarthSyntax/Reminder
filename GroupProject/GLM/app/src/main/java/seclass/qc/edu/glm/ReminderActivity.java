ec66053d8eefd77463e521b8a40e2617b79f437f
Loren Niu <loren.niu@macaulay.cuny.edu>
2018-11-21 18:39:36 -0500

    added .gitignore

--- GroupProject/GLM/app/src/main/java/seclass/qc/edu/glm/ReminderActivity.java
+++ GroupProject/GLM/app/src/main/java/seclass/qc/edu/glm/ReminderActivity.java
@@ -42,15 +42,6 @@ public class ReminderActivity extends AppCompatActivity {
         setContentView(R.layout.activity_reminders);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
     }
 
     public void submitAttributes(View v) {
